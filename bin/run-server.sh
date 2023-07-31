ef0e64643730da240117605ebf6a298fba40591c
requin <requin@debian.debian.requin.com>
2023-07-31 13:36:15 -0700

    script to run servers with local server repo checkouts

---
+++ bin/run-server.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+    echo "needs argument for controller dir"
+    exit 1
+fi
+
+function kill_process_by_name() {
+    pattern=$1
+    pids=$(ps aux | grep "$pattern" | grep -v grep | awk '{print $2}')
+    for pid in $pids; do
+        kill -9 $pid
+        echo "Killed process $pid"
+    done
+}
+
+kill_process_by_name "target/.*/server"
+kill_process_by_name "index.js"
+
+cd ControlpadServer
+cargo run &
+cd ../WebCP
+git checkout main
+here=$(pwd)
+cd $1
+controller_dir=$(pwd)
+cd $here
+ln -sfn $controller_dir /home/requin/controller
+node index.js
