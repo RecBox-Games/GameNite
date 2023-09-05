0d15466c92129ed70a3c079c402fde9183ae2761
Weston <westonkelliher@gmail.com>
2023-09-05 13:19:49 -0700

    build-rqn.sh; rqn build commit tracker; minor changes to other scripts

---
+++ bin/core_build_rqn.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+set -e
+
+BIN_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+base="$(cd $BIN_DIR/.. && pwd)"
+dest="$base/rqn/"
+
+cd $dest
+if [[ -n "$(git remote -v | grep westonkelliher)" ]]; then
+    echo "------------------------------------------------------------"
+    echo "--------------- YOU ARE USING THE WRONG REPO ---------------"
+    echo "----------------- delete rqn and try again -----------------"
+    echo "------------------------------------------------------------"
+fi
+
+if [ ! -z "$(ls $dest)" ]; then
+    rm -r $dest/*
+fi
+
+cd $base
+
+# Control Pad Server
+echo "----- ControlPadSever -----"
+cd ControlPadServer
+cargo build --release
+cp target/release/server $dest/cp_server
+cd ..
+
+# Node Web Server
+echo "----- WebCP -----"
+cd WebCP
+mkdir $dest/webcp
+cp index.js $dest/webcp/
+cp package.json $dest/webcp/
+cd ..
+
+# System Software (Loader and System)
+echo "----- SystemApps -----"
+cd SystemApps
+cargo build --release
+mkdir -p $dest/controller
+cp -r controller/controller $dest/
+cp target/release/loader $dest/
+cp target/release/system $dest/
+cp -r resources $dest/
+cd ..
+
+# Copy Scripts and Setup Files
+echo "----- Console Files -----"
+cd rqn-scripts
+echo "cp console-files/* $dest/"
+cp console-files/.bashrc $dest/
+cp console-files/.xinitrc $dest/
+cp console-files/* $dest/
+cd ..
+
+# Make the games dir
+mkdir $dest/games
+touch $dest/games/.gitkeep
