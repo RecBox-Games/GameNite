9b26b43e8d949823b1d6c2944fa73e265bac8d69
Weston <westonkelliher@gmail.com>
2023-10-28 16:56:19 -0700

    gamenite-games-testing by default for gcp-upload.sh

--- bin/gcp-upload.sh
+++ bin/gcp-upload.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-if [[ $# -ne 3 ]]; then
-    echo "usage: gcp-upload <bucket-name> <game-name> <directory>"
+if [[ $# -lt 2 ]]; then
+    echo "usage: gcp-upload <game-name> <directory> [<bucket-name>]"
+    echo "- default bucket name is gamenite-games-testing"
     exit 1
 fi
+
+game=$1
+dir=$2
+bucket=${3-gamenite-games-testing}
 
 chillin="true"
 function check_for {
@@ -13,11 +18,11 @@ function check_for {
     fi
 }
 
-cd $3
+cd $dir
 
-# check for preview.*, icon.*, description.txt, game/, game/meta.txt, game/controller/
-check_for "preview*" 
-check_for "icon*"
+# check for preview.png, icon.png, description.txt, game/, game/meta.txt, game/controller/
+check_for "preview.png" 
+check_for "icon.png"
 check_for "description.txt"
 check_for "game"
 check_for "game/meta.txt"
@@ -34,10 +39,12 @@ tar -czf ../game.tar.gz $(ls -a --color=never | grep -v "^\.\.\?$")
 cd ..
 
 echo "uploading files"
-gsutil cp ./icon* gs://$1/$2/icon
-gsutil cp ./preview* gs://$1/$2/preview
-gsutil cp ./description.txt gs://$1/$2/description
-gsutil setmeta -h "Cache-Control:no-cache, max-age=10" gs://$1/$2/game
-gsutil cp ./game.tar.gz gs://$1/$2/game
+gsutil mkdir gs://$bucket/$game
+gsutil cp ./icon.png gs://$bucket/$game/icon
+gsutil cp ./icon.png gs://$bucket/$game/icon.png
+gsutil cp ./preview.png gs://$bucket/$game/preview
+gsutil cp ./description.txt gs://$bucket/$game/description
+gsutil setmeta -h "Cache-Control:no-cache, max-age=10" gs://$bucket/$game/game
+gsutil cp ./game.tar.gz gs://$bucket/$game/game
 
 echo done
