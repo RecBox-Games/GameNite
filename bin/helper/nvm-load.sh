11ca96338f00c2243b93072811f8cc10b6e143b4
Safwaan Menk <41082275+safwaan-menk@users.noreply.github.com>
2024-08-22 20:13:01 -0700

    added LICENSE.txt and copyright to .sh files

--- bin/helper/nvm-load.sh
+++ bin/helper/nvm-load.sh
@@ -1,3 +1,20 @@
+# Copyright 2022-2024 RecBox, Inc.
+#
+# This file is part of the rqn repository.
+#
+# The scripts in rqn are free software: you can redistribute them and/or modify
+# them under the terms of the GNU General Public License as published by the 
+# Free Software Foundation, either version 3 of the License, or (at your option)
+# any later version.
+# 
+# The scripts in rqn are distributed in the hope that they will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+# more details.
+# 
+# You should have received a copy of the GNU General Public License along with
+# rqn. If not, see <https://www.gnu.org/licenses/>.
+
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # Load nvm
 
