cf5117b56bbaf24f06776378a347094ef8baf25f
Safwaan Menk <41082275+safwaan-menk@users.noreply.github.com>
2024-08-26 16:18:45 -0700

    Add LICENSE.txt, README.md, and notices on source files (#8)

--- bin/pull.sh
+++ bin/pull.sh
@@ -1,3 +1,19 @@
+# Copyright 2022-2024 RecBox, Inc.
+#
+# This file is part of the GameNite repository.
+#
+# GameNite is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by the 
+# Free Software Foundation, either version 3 of the License, or (at your option)
+# any later version.
+# 
+# GameNite is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+# more details.
+# 
+# You should have received a copy of the GNU General Public License along with
+# GameNite. If not, see <https://www.gnu.org/licenses/>.
 
 pull_repo() {
     start_dir=$(pwd)
@@ -14,7 +30,6 @@ pull_repo() {
     echo
     cd $start_dir
 }
-
 
 pull_repo ./
 pull_repo rqn
--- bin/status.sh
+++ bin/status.sh
@@ -1,3 +1,20 @@
+# Copyright 2022-2024 RecBox, Inc.
+#
+# This file is part of the GameNite repository.
+#
+# GameNite is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by the 
+# Free Software Foundation, either version 3 of the License, or (at your option)
+# any later version.
+# 
+# GameNite is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+# more details.
+# 
+# You should have received a copy of the GNU General Public License along with
+# GameNite. If not, see <https://www.gnu.org/licenses/>.
+
 
 status_repo() {
     start_dir=$(pwd)
@@ -17,7 +34,6 @@ status_repo() {
     echo
     cd $start_dir
 }
-
 
 status_repo ./
 status_repo rqn-scripts
